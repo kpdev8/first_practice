8f60f8e40344dc7902cd4e5385c6d7dfb3138df2
kpdev8 <79795475+kpdev8@users.noreply.github.com>
2021-04-17 19:37:28 +0900

    commit first!

---
+++ src/first_practice/first_Practice.java
@@ -0,0 +1,25 @@
+package first_practice;
+
+public class first_Practice {
+
+	public static void main(String args[]) {	//javaを実行するために必要な形式的なもの
+		
+		System.out.println("hello world");
+		
+//複数行出力するプログラム
+		System.out.println("私の名前はKPです。");	//文字を出力する+改行
+		System.out.println("1たす1は");
+		System.out.println(1+1);
+		System.out.println("です。");
+		
+		System.out.println("4わる2は");
+		System.out.println(4/2);
+		System.out.println("です。");		
+
+//演算プログラム		
+
+		System.out.println(3*5);
+		
+
+	}
+}
